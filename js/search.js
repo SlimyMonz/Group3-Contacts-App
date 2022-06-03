f75a1edf04c12a0074c73f2ea84661c2489155f5
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-06-03 12:23:55 -0400

    Files downloaded from the server and uploaded to github.

--- js/search.js
+++ js/search.js
@@ -25,22 +25,17 @@ function doSearch()
             if (this.readyState == 4 && this.status == 200)
             {
                 document.getElementById("contact_display_box").innerHTML = "Contact(s) has been retrieved";
-                let jsonObject = JSON.parse( xhr.responseText );
+		console.log( xhr.responseText + "bruh mmnt extravaganza");
+                var  jsObjectArr = JSON.parse(xhr.responseText);
 
-                console.log(jsonObject.results);
 
-                for( let i=0; i<jsonObject.results.length; i++ )
+                for( let i in jsObjectArr )
                 {
-                    contactList += jsonObject.results[i];
-
-                    if( i < jsonObject.results.length - 1 )
-                    {
-                        contactList += "<br />\r\n";
-                    }
+		    console.log(jsObjectArr[i]);
+                    contactList += jsObjectArr[i];
                 }
 
-                document.getElementsByTagName("p")[0].innerHTML = contactList; //if not working try [3] instead of [0]
-
+                document.getElementById("contact_display_box").innerHTML = contactList; 
                 alert(contactList); //this is for testing if the search actually works even if we can't display it
             }
         };
