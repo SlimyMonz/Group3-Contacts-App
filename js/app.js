4cb6dbe47628f6c417f100f97234439b3acac98f
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-06-02 21:27:48 -0400

    impliment modal on contacts.html
    also cleaned up app.js and removed redundant styles.css

--- js/app.js
+++ js/app.js
@@ -7,37 +7,6 @@ let userId = 0;
 let firstName = "";
 let lastName = "";
 
-function showAlert() {
-    var myText = "Did you think there would be somthing here?";
-    alert (myText);
-}
-
-// open pop-up menu for add contact
-function show_add_contact()
-{
-  var static_body = document.getElementById("static_body");
-  static_body.style = "opacity: .33;"
-
-  var add_contact_form = document.getElementById("add_contact_form");
-  add_contact_form.style = "display: block;"
-
-  // static_body_click_behavior();
-}
-
-function submit_form_data(){
-  addContact();
-  close_add_box();
-  // var static_body = document.getElementById("static_body");
-  // static_body.onclick = "";
-}
-
-// close pop up menu. right now this is more of a cancel than a submit
-function close_add_box(){
-  var add_contact_form = document.getElementById("add_contact_form");
-  var static_body = document.getElementById("static_body");
-  add_contact_form.style = "display: none;";
-  static_body.style = "opacity: 1;";
-}
 function clear()
 {
   $("#first_name").val("");
