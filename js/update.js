ba2bcd85b4dad37a7ac720b170e3a57459676e4d
fayrra <fayrra@hotmail.com>
2022-06-05 13:59:39 -0400

    edit implementation

---
+++ js/update.js
@@ -0,0 +1,102 @@
+//important variable for verifying that the modal was canceled rather than submitted 1 = canceled
+var theGreatEscape = 0;
+
+function cancelEdit()
+{
+
+    theGreatEscape = 1;
+    $("#getModal").modal("hide"); //hides modal, this function runs whenever the modal is canceled, only on buttonclick
+                                  //this is why this modal is set so you can't exit any other way (keyboard, backdrop)
+
+}
+
+// to self: don't close modal on cancel, close it on submit
+
+ async function showModal() //where the magic happens
+{
+
+    $("#getModal").modal("show"); //shows modal ->
+    return new Promise(resolve => //-> promises to resolve the await only when ->
+
+        $("#getModal").on("hidden.bs.modal", () => // -> the modal is hidden ->
+        {
+            resolve();  // -> regardless of how it was hidden, we now have waited and either set greatescape or didn't
+        }
+        )
+     
+    );
+
+}
+
+
+ async function editContact(info)
+{
+
+
+    let contactName = '';
+    let contactPhone = '';
+    let contactEmail = '';
+
+    let user_Id = info.data; //gets contact's id
+    alert( "user id is: " + user_Id);
+
+    showModal(); //pops up the modal
+
+    await showModal(); //waits at this point before running any more of this function's code
+
+    alert("escape: " + theGreatEscape);
+    alert("thank goodness we made it this far");
+    
+
+
+    if(theGreatEscape == 1) //we do NOT want to continue because user canceled
+    {
+        theGreatEscape = 0;
+        return; //nope out
+    }
+   
+    //user has decided to actually edit, gets values inputted in modal
+    contactName = $("#editName").val();
+    contactPhone = $("#editPhone").val();
+    contactEmail = $("#editEmail").val();
+
+    alert("name: " + contactName + " phone: " + contactPhone + " email: " + contactEmail + " id: " + user_Id);
+
+    //save into object
+    let editedContact = 
+    {
+        newPhone: contactPhone,
+        newEmail: contactEmail,
+        name: contactName,
+        userId: user_Id
+        
+    };
+   
+
+    //JSON.stringify(object) etc.
+    let jsonPayload = JSON.stringify( editedContact );
+
+    let url = urlBase + '/Update.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        xhr.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                
+                // repaint page
+                //alert("does this work?");
+                doSearch();
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        //document.getElementById("colorAddResult").innerHTML = err.message;
+    }
+}
