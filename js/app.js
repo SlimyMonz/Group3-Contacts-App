99f53888db1fba28ccabb4cf5a4ed521b3913b35
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-06-12 16:06:24 -0400

    Made contact appear on add
    needs styling and grey out add delete buttons

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 const urlBase = 'http://group3.rocks/LAMPAPI';
 const extension = 'php';
 
+//arra for recently added table;
+let recently_added_arr = [];
+
 let userId = "";
 let firstName = "";
 let lastName = "";
